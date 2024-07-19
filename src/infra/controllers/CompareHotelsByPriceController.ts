2c6d83e24a8879d94f7943467e65643fb76b2917
alissonFabricio04 <alissonfabricio.dev@gmail.com>
2024-07-19 13:11:26 -0300

    mudando para o req.query

--- src/infra/controllers/CompareHotelsByPriceController.ts
+++ src/infra/controllers/CompareHotelsByPriceController.ts
@@ -17,7 +17,11 @@ export default class CompareHotelsByPriceController {
 
   async handle(req: Request, res: Response) {
     try {
-      const output = await this.useCase.handle(req.body)
+      const input = {
+        room01Id: req.query.room01Id as string,
+        room02Id: req.query.room02Id as string
+      }
+      const output = await this.useCase.handle(input)
       return res
         .status(200)
         .json(output)
